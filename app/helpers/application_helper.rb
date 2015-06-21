8407f0dc8930fada129eb506d794b5ae2581c905
Mandeep Ahlawat <mandeep052@gmail.com>
2015-06-21 16:13:53 +0530

    form changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def form_styles form_attribute
+		"font-family: #{form_attribute.font_family}; color: #{form_attribute.color}; background-color: #{form_attribute.background_color}"	if form_attribute
+	end
+
+	def input_styles form_attribute
+		"width: #{form_attribute.width}; border: #{form_attribute.border}; color: #{form_attribute.color}; font-family: #{form_attribute.font_family};" if form_attribute
+	end
+
+	def button_styles form_attribute
+		"background-color: #{form_attribute.button_color}; color: #{form_attribute.color}; font-family: #{form_attribute.font_family};" if form_attribute
+	end
 end
