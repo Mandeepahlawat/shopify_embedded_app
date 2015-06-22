c6e2a4d0be195f9cf9aa89a83d3353f7fc92ee39
Mandeep Ahlawat <mandeep052@gmail.com>
2015-06-23 01:18:33 +0530

    render path after recommendation is sent fix

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -21,7 +21,8 @@ class RecommendationsController < ApplicationController
   	@recommendation = @customer.recommendations.build(recommendation_params)
   	if @recommendation.save
       @recommendation.send_recommendation_email
-  		redirect_to new_recommendation_path(shop: @shop.shopify_domain), notice: "Product Recommendation is sent."
+      redirect_to "/#{@shop.shopify_domain}", notice: "Product Recommendation is sent."
+  		#redirect_to new_recommendation_path(shop: @shop.shopify_domain), notice: "Product Recommendation is sent."
   	else
   		render "new"
   	end
