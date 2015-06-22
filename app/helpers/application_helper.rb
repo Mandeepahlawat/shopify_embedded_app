8e0224dbf654f966315f01df5de1604d57469235
Mandeep Ahlawat <mandeep052@gmail.com>
2015-06-23 01:08:39 +0530

    + for nil error fix when no form attribute present

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,25 @@
 module ApplicationHelper
 	def form_styles form_attribute
-		"font-family: #{form_attribute.font_family}; color: #{form_attribute.color}; background-color: #{form_attribute.background_color};"	if form_attribute
+		if form_attribute
+			"font-family: #{form_attribute.font_family}; color: #{form_attribute.color}; background-color: #{form_attribute.background_color};"
+		else
+			""
+		end
 	end
 
 	def input_styles form_attribute
-		"width: #{form_attribute.width}; border: #{form_attribute.border}; color: #{form_attribute.color}; font-family: #{form_attribute.font_family};" if form_attribute
+		if form_attribute
+			"width: #{form_attribute.width}; border: #{form_attribute.border}; color: #{form_attribute.color}; font-family: #{form_attribute.font_family};"
+		else
+			""
+		end
 	end
 
 	def button_styles form_attribute
-		"background-color: #{form_attribute.button_color}; color: #{form_attribute.color}; font-family: #{form_attribute.font_family};" if form_attribute
+		if form_attribute
+			"background-color: #{form_attribute.button_color}; color: #{form_attribute.color}; font-family: #{form_attribute.font_family};"
+		else
+			""
+		end
 	end
 end
